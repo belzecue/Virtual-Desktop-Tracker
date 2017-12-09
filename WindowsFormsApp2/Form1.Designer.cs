b09c45a95f6d4c5e4b64c11ad140d7e95d99f3ea
Andrew Ferguson <fergicide@gmail.com>
2017-12-09 19:20:51 +0800

    Add ini file class

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApp2
+﻿namespace VDTracker
 {
 	partial class Form1
 	{
--- WindowsFormsApp2/VDExampleWindow.cs
+++ WindowsFormsApp2/VDExampleWindow.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using WindowsFormsApp2;
+using VDTracker;
 
 namespace VDMBackgroundManager
 {
