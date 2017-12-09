e1b12b6d67b58b9ae0417886c65764eaa1c2702d
Andrew Ferguson <fergicide@gmail.com>
2017-12-09 15:13:43 +0800

    Add missing icons

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -33,6 +33,12 @@
 			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
 			this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
 			this.notifyIcon3 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon4 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon5 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon6 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon7 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon8 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.notifyIcon9 = new System.Windows.Forms.NotifyIcon(this.components);
 			this.SuspendLayout();
 			// 
 			// notifyIcon1
@@ -53,6 +59,42 @@
 			this.notifyIcon3.Text = "notifyIcon1";
 			this.notifyIcon3.Visible = true;
 			// 
+			// notifyIcon4
+			// 
+			this.notifyIcon4.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon4.Icon")));
+			this.notifyIcon4.Text = "notifyIcon1";
+			this.notifyIcon4.Visible = true;
+			// 
+			// notifyIcon5
+			// 
+			this.notifyIcon5.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon5.Icon")));
+			this.notifyIcon5.Text = "notifyIcon1";
+			this.notifyIcon5.Visible = true;
+			// 
+			// notifyIcon6
+			// 
+			this.notifyIcon6.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon6.Icon")));
+			this.notifyIcon6.Text = "notifyIcon1";
+			this.notifyIcon6.Visible = true;
+			// 
+			// notifyIcon7
+			// 
+			this.notifyIcon7.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon7.Icon")));
+			this.notifyIcon7.Text = "notifyIcon1";
+			this.notifyIcon7.Visible = true;
+			// 
+			// notifyIcon8
+			// 
+			this.notifyIcon8.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon8.Icon")));
+			this.notifyIcon8.Text = "notifyIcon1";
+			this.notifyIcon8.Visible = true;
+			// 
+			// notifyIcon9
+			// 
+			this.notifyIcon9.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon9.Icon")));
+			this.notifyIcon9.Text = "notifyIcon1";
+			this.notifyIcon9.Visible = true;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -69,6 +111,12 @@
 		private System.Windows.Forms.NotifyIcon notifyIcon1;
 		private System.Windows.Forms.NotifyIcon notifyIcon2;
 		private System.Windows.Forms.NotifyIcon notifyIcon3;
+		private System.Windows.Forms.NotifyIcon notifyIcon4;
+		private System.Windows.Forms.NotifyIcon notifyIcon5;
+		private System.Windows.Forms.NotifyIcon notifyIcon6;
+		private System.Windows.Forms.NotifyIcon notifyIcon7;
+		private System.Windows.Forms.NotifyIcon notifyIcon8;
+		private System.Windows.Forms.NotifyIcon notifyIcon9;
 	}
 }
 
